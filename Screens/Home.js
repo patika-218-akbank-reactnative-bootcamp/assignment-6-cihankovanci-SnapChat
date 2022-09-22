a0fadbd644a3a379b946f0752b510a596afc493d
cihankovanci <cihankovanci@gmail.com>
2022-09-22 04:25:32 +0300

    feat: add expo image picker

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,7 +1,10 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { auth } from '../firebase'
 import CustomButton from '../components/CustomButton.js/CustomButton';
+
+import * as ImagePicker from 'expo-image-picker';
+
 
 const handleSelect = () =>{
    
@@ -12,13 +15,45 @@ const handleSelect = () =>{
 };
 
 const Home = () => {
+    const [image, setImage] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+
+    useEffect(()=> {
+        (async () => {
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+        })();
+    }, []);
+
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+      };
+
+      if(hasGalleryPermission === false){
+        return <Text>No access to Internal storage</Text>
+      }
+
   return (
     <View style={styles.container}>
       <Text>Email: {auth.currentUser?.email}</Text>
 
      
-      <CustomButton title="Select from Library" onPress={handleSelect} />
+      <CustomButton title="Select from Library" onPress={pickImage} />
+      
       <CustomButton title="Open Camera" onPress={openCamera} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
   )
 }
