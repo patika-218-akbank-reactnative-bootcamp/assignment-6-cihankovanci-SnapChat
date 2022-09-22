e031d94aec2981e337f9f5d0996865c31809d068
cihankovanci <cihankovanci@gmail.com>
2022-09-22 03:54:57 +0300

    feat: add react native maps

--- Screens/Map/Map.js
+++ Screens/Map/Map.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+       <MapView style={styles.map} />
     </View>
   )
 }
 
 export default Map
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      },
+})
